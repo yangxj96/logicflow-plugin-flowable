e72da9045b4408d25fad49a77d0ba9f2299117a5
yangxj96 <yangxj96@gmail.com>
2026-07-26 00:30:12 +0800

    chore: bump version to 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*
- * logicflow-plugin-flowable v0.0.5
+ * logicflow-plugin-flowable v0.1.0
  * Copyright 2025 Yang XJ. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
